c4e259a20680c566e09b67a0349afe30222dd5e6
zl1732 <zl1732@nyu.edu>
2025-09-21 21:31:55 +0800

    add file

---
+++ 47.Permutations II.py
@@ -0,0 +1,64 @@
+#
+# @lc app=leetcode id=47 lang=python3
+# @lcpr version=30201
+#
+# [47] Permutations II
+#
+# https://leetcode.com/problems/permutations-ii/description/
+#
+# algorithms
+# Medium (62.08%)
+# Likes:    8939
+# Dislikes: 158
+# Total Accepted:    1.2M
+# Total Submissions: 1.9M
+# Testcase Example:  '[1,1,2]'
+#
+# Given a collection of numbers, nums, that might contain duplicates, return
+# all possible unique permutations in any order.
+# 
+# 
+# Example 1:
+# 
+# Input: nums = [1,1,2]
+# Output:
+# [[1,1,2],
+# ⁠[1,2,1],
+# ⁠[2,1,1]]
+# 
+# 
+# Example 2:
+# 
+# Input: nums = [1,2,3]
+# Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= nums.length <= 8
+# -10 <= nums[i] <= 10
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# [1,1,2]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [1,2,3]\n
+# @lcpr case=end
+
+#
+
---
+++ 78.子集.py
@@ -0,0 +1,65 @@
+#
+# @lc app=leetcode.cn id=78 lang=python3
+# @lcpr version=30201
+#
+# [78] 子集
+#
+# https://leetcode.cn/problems/subsets/description/
+#
+# algorithms
+# Medium (82.36%)
+# Likes:    2538
+# Dislikes: 0
+# Total Accepted:    1.1M
+# Total Submissions: 1.3M
+# Testcase Example:  '[1,2,3]'
+#
+# 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+# 
+# 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+# 
+# 
+# 
+# 示例 1：
+# 
+# 输入：nums = [1,2,3]
+# 输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+# 
+# 
+# 示例 2：
+# 
+# 输入：nums = [0]
+# 输出：[[],[0]]
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 1 <= nums.length <= 10
+# -10 <= nums[i] <= 10
+# nums 中的所有元素 互不相同
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# [1,2,3]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [0]\n
+# @lcpr case=end
+
+#
+
